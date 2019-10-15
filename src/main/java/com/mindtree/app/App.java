2de04a9febb909339cbf1ee0ffc695c77a3451fb
Manasi <manasi.panda@mindtree.com>
2019-10-15 23:31:38 +0200

    Added changes

---
+++ src/main/java/com/mindtree/app/App.java
@@ -0,0 +1,35 @@
+package com.mindtree.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello World!";
+	private String a = "TEST STRING";
+	private String b = "TEST STRING";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+	private int add(int a, int b) {
+	    return a+b;
+	}
+	public int subs(int a, int b) {
+	    return a-b;
+	}
+	private int add2(int a, int b) {
+	    return a+b;
+	}
+	private int add3(int a, int b) {
+	    return a+b;
+	}
+
+}
